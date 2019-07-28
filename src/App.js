41b6da53518f1f3d84eaa01c2f466a3ba7b55721
Ryan Culhane <rpc21@duke.edu>
2019-07-28 13:49:54 -0400

    pushing to master to see if deploy changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Redirect } from 'react-router-dom';
+import { HashRouter, Route, Redirect, Switch } from 'react-router-dom';
 import PosterPage from './pages/poster-page/poster-page.component';
 import ReferencesPage from './pages/references-page/references-page.component';
 import SampleAudioPage from './pages/sample-audio-page/sample-audio-page.component';
@@ -11,13 +11,13 @@ class App extends Component {
     return (
       <div>
         <Header />
-        <h1>Testing</h1>
-        <HashRouter basename='/data-plus-results/'>
+        <Switch>
           <Route exact={true} path='/' component={HomePage} />
-          <Route exact={true} path='/data-plus-results/poster' component={PosterPage} />
-          <Route exact={true} path='/data-plus-results/references' component={ReferencesPage} />
-          <Route exact={true} path='/data-plus-results/sample_audio' component={SampleAudioPage} />
-        </HashRouter>
+          <Route exact={true} path='/poster' component={PosterPage} />
+          <Route exact={true} path='/references' component={ReferencesPage} />
+          <Route exact={true} path='/sample_audio' component={SampleAudioPage} />
+        </Switch>
+
       </div>
     );
   }
