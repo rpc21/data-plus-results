f6ee615a8221b68e160c5cf5475d00f9dc66c810
Ryan Culhane <rpc21@duke.edu>
2019-07-28 14:28:33 -0400

    testing navbar again

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,30 +8,30 @@ import './header.component.scss';
 
 const Header = () => {
     return (
-        <div className='header'>
-            <Link className='logo-container' to='/'>
-                <img src={logo} alt='logo' className='logo' />
-            </Link>
-            <div className='options'>
-                <Link className='option' to='/poster'>
-                    POSTER
-                </Link>
-                <Link className='option' to='/references'>
-                    REFERENCES
-                </Link>
-                <Link className='option' to='/sample_audio'>
-                    SAMPLES
-                </Link>
-            </div>
-        </div>
-        // <Navbar bg="light" variant="light" className='navbar'>
-        //     <Navbar.Brand href="/">Speech Emotion Analysis</Navbar.Brand>
-        //     <Nav className="mr-auto">
-        //         <Nav.Link href="/references">References</Nav.Link>
-        //         <Nav.Link href="/poster">Poster</Nav.Link>
-        //         <Nav.Link href="/sample_audio">Sample Audio</Nav.Link>
-        //     </Nav>
-        // </Navbar>
+        // <div className='header'>
+        //     <Link className='logo-container' to='/'>
+        //         <img src={logo} alt='logo' className='logo' />
+        //     </Link>
+        //     <div className='options'>
+        //         <Link className='option' to='/poster'>
+        //             POSTER
+        //         </Link>
+        //         <Link className='option' to='/references'>
+        //             REFERENCES
+        //         </Link>
+        //         <Link className='option' to='/sample_audio'>
+        //             SAMPLES
+        //         </Link>
+        //     </div>
+        // </div>
+        <Navbar bg="light" variant="light" className='navbar'>
+            <Navbar.Brand href="/">Speech Emotion Analysis</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="/references">References</Nav.Link>
+                <Nav.Link href="/poster">Poster</Nav.Link>
+                <Nav.Link href="/sample_audio">Sample Audio</Nav.Link>
+            </Nav>
+        </Navbar>
     );
 }
 
