dbf900b71dd92fe2f2e53b02c09075b67b2b4764
Ryan Culhane <rpc21@duke.edu>
2019-07-28 14:16:29 -0400

    Tested google slides link functionality

--- src/pages/poster-page/poster-page.component.jsx
+++ src/pages/poster-page/poster-page.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './poster-page.component.scss';
 
 const PosterPage = () => {
-    return <h1>Page to display the poster</h1>;
+    return <iframe 
+        src="https://docs.google.com/presentation/d/e/2PACX-1vSzbaujc9Gvs4dLsjMObgXxe_4y8Dw50SU-LrOhlv6yqGNgVUsBDJYurmFPBGQSrklxJ-MKjh3aCCq-/embed?start=false&loop=true&delayms=60000" frameborder="0" width="700" height="600" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>;
 }
 
 export default PosterPage;
